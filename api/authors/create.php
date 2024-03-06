9b3b7c442e0d19cf81fc3acdb79886b0e0b1b767
John <katsuya245126@gmail.com>
2024-03-06 21:39:04 +0900

    Implement create method and page

---
+++ api/authors/create.php
@@ -0,0 +1,38 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Access-Control-Allow-Origin, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Author.php';
+
+    // Instantiate DB & Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate Author object
+    $author = new Author($db);
+
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (is_null($data) || empty($data->author)) {
+        echo json_encode(
+            array('message' => 'No info provided')
+        );
+        die();
+    }
+
+    $author->author = $data->author;
+
+    if($author->create()) {
+        echo json_encode(
+            array('message' => 'Post Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Post Not Created')
+        );
+    }
--- models/Author.php
+++ models/Author.php
@@ -22,15 +22,34 @@
         }
 
         public function read_single() {
-            $query = 'SELECT * FROM ' . $this->table . ' WHERE id = ? LIMIT 1';
+            $query = 'SELECT * FROM ' . $this->table . ' WHERE id = :id LIMIT 1';
 
             $stmt = $this->conn->prepare($query);
-            $stmt->bindParam(1, $this->id);
+            $stmt->bindParam(':id', $this->id);
 
             $stmt->execute();
 
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
             $this->author = $row['author'];
+        }
+
+        public function create() {
+            $query = 'INSERT INTO ' . $this->table . ' (author) VALUES (:author)';
+
+            $stmt = $this->conn->prepare($query);
+            
+            // Clean data
+            $this->author = htmlspecialchars(strip_tags($this->author));
+
+            $stmt->bindParam(':author', $this->author);
+
+            if($stmt->execute()) {
+                return true;
+            }
+
+            printf("Error: %s.\n", $stmt->error);
+
+            return false;
         }
     }
