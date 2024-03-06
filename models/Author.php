50012f39cfb34fbdab174f96a658f6d867ebdc13
John <katsuya245126@gmail.com>
2024-03-06 21:11:02 +0900

    Implement read_single method and page

--- models/Author.php
+++ models/Author.php
@@ -20,4 +20,17 @@
 
             return $stmt;
         }
+
+        public function read_single() {
+            $query = 'SELECT * FROM ' . $this->table . ' WHERE id = ? LIMIT 1';
+
+            $stmt = $this->conn->prepare($query);
+            $stmt->bindParam(1, $this->id);
+
+            $stmt->execute();
+
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            $this->author = $row['author'];
+        }
     }
