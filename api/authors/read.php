a4ccb199a4f7e5332dfbea8b47f5c1b1d9c018b9
John <katsuya245126@gmail.com>
2024-03-06 20:57:51 +0900

    Complete read.php for Author

---
+++ api/authors/read.php
@@ -0,0 +1,37 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Author.php';
+
+    // Instantiate DB & Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate Author object
+    $author = new Author($db);
+
+    $result = $author->read();
+    $num = $result->rowCount();
+
+    if($num > 0) {
+        $authors_arr = array();
+        $authors_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $author_item = array(
+                'id' => $id,
+                'author' => $author
+            );
+
+            array_push($authors_arr['data'], $author_item);
+        }
+
+        echo json_encode($authors_arr);
+    } else {
+        echo json_encode(array('message' => 'No posts found'));
+    }
