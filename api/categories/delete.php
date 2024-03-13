661cfe09a86c95bebac8f69cb92f75b2856037c3
John <katsuya245126@gmail.com>
2024-03-13 20:09:42 +0900

    Implement update and delete endpoint for categories

---
+++ api/categories/delete.php
@@ -0,0 +1,38 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: DELETE');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Access-Control-Allow-Origin, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
+
+    // Instantiate DB & Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate Category object
+    $category = new Category($db);
+
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (is_null($data) || empty($data->id)) {
+        echo json_encode(
+            array('message' => 'Missing Required Parameters')
+        );
+        die();
+    }
+
+    $category->id = $data->id;
+    
+    if($category->delete()) {
+        echo json_encode(
+            array('message' => 'Category Deleted')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Category Not Deleted')
+        );
+    }
---
+++ api/categories/update.php
@@ -0,0 +1,39 @@
+<?php
+    // Headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: PUT');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Methods, Content-Type, Access-Control-Allow-Origin, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
+
+    // Instantiate DB & Connect
+    $database = new Database();
+    $db = $database->connect();
+
+    // Instantiate Category object
+    $category = new Category($db);
+
+    // Get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    if (is_null($data) || empty($data->id)) {
+        echo json_encode(
+            array('message' => 'Missing Required Parameters')
+        );
+        die();
+    }
+
+    $category->id = $data->id;
+    $category->category = $data->category;
+
+    if($category->update()) {
+        echo json_encode(
+            array('message' => 'Category Updated')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Category Not Updated')
+        );
+    }
